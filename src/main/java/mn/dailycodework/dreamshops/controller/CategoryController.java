4b0277536b9ab29d40452f8d7ae1f91501bfcf34
zoljargal <zoljargal.b@nomin.net>
2025-04-15 18:48:22 +0800

    Updates

--- src/main/java/mn/dailycodework/dreamshops/controller/CategoryController.java
+++ src/main/java/mn/dailycodework/dreamshops/controller/CategoryController.java
@@ -1,5 +1,59 @@
 package mn.dailycodework.dreamshops.controller;
 
-public class CategoryController {
+import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import mn.dailycodework.dreamshops.exceptions.AlreadyExistsException;
+import mn.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import mn.dailycodework.dreamshops.model.Category;
+import mn.dailycodework.dreamshops.response.ApiResponse;
+import mn.dailycodework.dreamshops.service.category.ICategoryService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("category")
+public class CategoryController {
+    private final ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories() {
+        try {
+
+            List<Category> categories = categoryService.getAllCategories();
+
+            return ResponseEntity.ok(new ApiResponse("Founded", categories));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error", HttpStatus.INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category category) {
+        try {
+            Category theCategory = categoryService.createCategory(category);
+            return ResponseEntity.ok(new ApiResponse("Success", theCategory));
+        } catch (AlreadyExistsException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long categoryId) {
+        try {
+            Category category = categoryService.getCategoryById(categoryId);
+            return ResponseEntity.ok(new ApiResponse("Founded", category));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+        }
+    }
 }
--- src/main/java/mn/dailycodework/dreamshops/controller/ImageController.java
+++ src/main/java/mn/dailycodework/dreamshops/controller/ImageController.java
@@ -1,8 +1,13 @@
 package mn.dailycodework.dreamshops.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,9 +16,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import mn.dailycodework.dreamshops.dto.ImageDto;
+import mn.dailycodework.dreamshops.exceptions.ResourceNotFoundException;
+import mn.dailycodework.dreamshops.model.Image;
 import mn.dailycodework.dreamshops.response.ApiResponse;
 import mn.dailycodework.dreamshops.service.image.IImageService;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @RestController
@@ -33,5 +46,47 @@ public class ImageController {
                     .body(new ApiResponse("Upload failed", e.getMessage()));
 
         }
+    }
+
+    @GetMapping("/image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId) throws SQLException {
+        Image image = imageService.getImageById(imageId);
+        ByteArrayResource resource = new ByteArrayResource(
+                image.getImage().getBytes(1, (int) image.getImage().length()));
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachemnt; filename=\"" + image.getFileName() + "\"")
+                .body(resource);
+    }
+
+    @PutMapping("image/{imageId}/update")
+    public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.updateImage(file, imageId);
+                return ResponseEntity.ok(new ApiResponse("Successfully updated!", image));
+            }
+        } catch (ResourceNotFoundException e) {
+            // return ResponseEntity.status().body(new ApiResponse(e.getMessage(), null));
+            return ResponseEntity.status(404).body(new ApiResponse(e.getMessage(), null));
+        }
+        return ResponseEntity.status(500).body(new ApiResponse("Update Failed", 500));
+
+    }
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId) {
+        try {
+            Image image = imageService.getImageById(imageId);
+            if (image != null) {
+                imageService.deleteImageById(imageId);
+                return ResponseEntity.ok(new ApiResponse("Successfully deleted!", 200));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(new ApiResponse(e.getMessage(), null));
+        }
+
+        return ResponseEntity.status(500).body(new ApiResponse("Delete Failed", 500));
     }
 }
